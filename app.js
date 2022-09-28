d0d9d2a3750a7e834f38a54afa6babaa046224b1
Asma Kacem <asma.kacem217@gmail.com>
2022-09-28 13:57:21 +0100

    Level 3 - Hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5= require("md5");
 
 const app=express();
 
@@ -19,8 +19,7 @@ const userSchema = new mongoose.Schema ({
     password:String
 });
 
-const secret="thisismysecret";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -41,7 +40,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -55,7 +54,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({email:username},function(err,foundUser){
         if(foundUser){
             if(foundUser.password===password){
